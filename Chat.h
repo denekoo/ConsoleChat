553978d9d73d80d2d4fd953f70ca01209b2443c9
denekoo <denek@inbox.ru>
2023-09-26 11:47:58 +0600

    26.09

--- Chat.h
+++ Chat.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <vector>
@@ -19,19 +19,19 @@ public:
 	Chat();
 	~Chat();
 
-	int numInput();
-	int getUserCounter();
-	void addUser();
-	void registrator();
-	bool checkUser(std::string userName);
-	bool checkPassword(User user, int password);
-	bool userRegister();
-	bool userLogin();
-	void showUserList();
-	User selectUser();
-	bool chatMessage(User user);
-	void privateChat();
-	void chat();
+	int numInput();									// Функция проверки ввода, с защитой от некорректного ввода
+	int getUserCounter();							// Возвращает количетво зарегистрированных пользователей
+	void addUser();									// Добавляет экземпляр класса User в массив, увеличивает счетчик пользователей
+	void registrator();								// Регисратор, выполняет все функции по регистрации и входу пользователей
+	bool checkUser(std::string userName);			// Функция проверки имени пользователя из массива пользователей, возвращает false, если имя совпало
+	bool checkPassword(User user, int password);	// Функция проверки пароля, возвращает true если пароль верный
+	bool userRegister();							// Выполняет действия регистрации нового пользователя, при этом выполняя проверку имени
+	bool userLogin();								// Функция входа зарегистрированного пользователя, с проверкой имения и пароля на совпадение  
+	void showUserList();							// Вывод списка всех пользователей с указанием статуса авторизации и активности пользователей
+	User selectUser();								// Функция выбора активного пользователя для написания сообщения
+	bool chatMessage(User user);					// Функция отправки сообщения от выбранного пользователя
+	void privateChat();								// Приватный чат для двух пользователей
+	void chat();									// Общий чат для общения всех зарегистрированных пользователей
 };
 
 
